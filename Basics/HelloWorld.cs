aa6f7dcbc62be13c3c315b405584105f873e6170
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-17 17:18:47 +0530

    Add project files.

---
+++ Basics/HelloWorld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics
+{
+    class HelloWorld
+    {
+        
+       
+      public void PrintingHelloWorld()
+        {
+            Console.WriteLine("hello");
+        }
+
+
+}
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CsharpLearningProject.Basics;
+
+HelloWorld helloWorld = new HelloWorld();
+helloWorld.PrintingHelloWorld ();
+
