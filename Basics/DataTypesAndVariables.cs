8e35e977a4ea70aee3270e495c7c64b8286615b5
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-17 17:28:19 +0530

    datatypes

---
+++ Basics/DataTypesAndVariables.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics
+{
+     class DataTypesAndVariables
+    {
+        int a = 10;
+        int b= 20;
+
+        public void AddTwoIntegerValues()
+        {
+            int c = a + b;
+            Console.WriteLine(c);
+        }
+    }
+}
