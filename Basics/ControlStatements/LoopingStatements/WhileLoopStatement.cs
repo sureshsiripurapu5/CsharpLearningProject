1fee285a6b46b15bd238d6a76764c2496d3ff207
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-18 16:51:48 +0530

    WhileStatement

---
+++ Basics/ControlStatements/LoopingStatements/WhileLoopStatement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.ControlStatements.LoopingStatements
+{
+     class WhileLoopStatement
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine(i);
+                i = i + 1;
+            }
+
+        }
+    }
+}
+
+/* while is a iteration statement which is used when the iteration is not fixed*/
