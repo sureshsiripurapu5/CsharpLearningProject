55b25d062dbcb57c21636db26a80f0e1a2c5b460
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-18 17:18:48 +0530

    CallByValue

---
+++ Basics/CallByValue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics
+{
+    class CallByValue
+    { 
+        public void CallByValueExample(int val)
+        {
+            val = val * val;
+            Console.WriteLine("Manupulating the value:");
+            Console.WriteLine(val);
+        }
+
+
+
+
+        public static void Main(string[] args)
+        {
+            int originlValue = 25;
+            CallByValue cb = new CallByValue();
+            Console.WriteLine("Original value:" + originlValue);
+            cb.CallByValueExample(originlValue);
+            Console.WriteLine("After manipulation original value:"+originlValue );
+
+
+        }
+    }
+}
+
+/* In call by value or pass by value we are passing the copy of the value not the reference,
+ * so if we make changes for the copy variable the original value wont gets modified */
