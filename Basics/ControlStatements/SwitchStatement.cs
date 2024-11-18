5b0b333fa1a66e37e720020ce2253374b94943ad
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-18 16:38:29 +0530

    switch statement

---
+++ Basics/ControlStatements/SwitchStatement.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.ControlStatements
+{
+     class SwitchStatement
+    {
+        
+
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first value:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second value:");
+            int j = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the operation to perform:+,-,*,/");
+            char c= char.Parse(Console.ReadLine());
+
+
+            switch (c)
+            {
+                case '+':
+                    Console.WriteLine("Addition of two numbers are:" + (i + j));
+                    break;
+
+                case '-':
+                    Console.WriteLine("Subraction of two numbers are:"+ (i-j));
+                    break;
+
+                case '*':
+                    Console.WriteLine("Multiplication of two numbers are :" + (i * j));
+                    break;
+
+                case '/':
+                    Console.WriteLine("Division of two numers are:" + (i / j));
+                    break;
+
+                default :
+                    Console.WriteLine("Invalid operation ");
+                    break;
+
+            } 
+                
+                
+
+        }
+
+    }
+}
