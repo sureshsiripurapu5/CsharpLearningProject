2991b7a7e8c67c9392887e87f57c6ae1ff3e01ee
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-18 17:00:17 +0530

    Foor Loop

---
+++ Basics/ControlStatements/LoopingStatements/ForLoopStatement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.ControlStatements.LoopingStatements
+{
+     class ForLoopStatement
+    {
+        public static void Main(string[] args)
+        {
+            for(int i=1;i<=10;i=i+1) 
+                Console.WriteLine(i);
+
+            //nested loop
+
+             for(int i=1; i <= 5; i++)
+            {
+                for(int j=1; j<=5; j++)
+                {
+                    Console.WriteLine(i+" "+j);
+                }
+            }
+
+        }
+    }
+}
+
+/* Foor loop is used to iterate the program several times,
+   it is used when the iteration is fixed */
