fc4f4850d75c4b533bc3f35ef041997f6227c504
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-18 16:46:58 +0530

    Do while statement

---
+++ Basics/ControlStatements/LoopingStatements/DoWhileLoopStatement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.ControlStatements.LoopingStatements
+{
+    class DoWhileLoopStatement
+    {
+        public static void Main(string[] args)
+        {
+            int i = 1;
+
+            do                                   
+            {
+                Console.WriteLine(i);
+                i = i + 1;
+            } while (i <= 10);
+        }
+    }
+}
+
+/* The do while is the iteration statement which is used when the numbers of iterations is not fixed, 
+ * it will always executes once without checking the condition */
