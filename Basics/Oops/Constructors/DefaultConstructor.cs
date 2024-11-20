9076c488d7a6987fa0f52f5a941d2009b006da18
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-20 21:57:21 +0530

    Default constructor

---
+++ Basics/Oops/Constructors/DefaultConstructor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.Oops.Constructors
+{
+     class DefaultConstructor
+    {
+        public DefaultConstructor() {
+            Console.WriteLine("This is a default constructor it will be automatically inviked when we create the object");
+
+        }
+        public static void Main(string[] args)
+        {
+            DefaultConstructor defaultConstructor = new DefaultConstructor();
+            DefaultConstructor defaultConstructor1 = new DefaultConstructor();
+        }
+    }
+}
