b75279ae285ae109b7d645292454e8ad60daba75
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-20 22:21:03 +0530

    destructor

---
+++ Basics/Oops/Constructors/Destructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.Oops.Constructors
+{
+     class Destructor
+    {
+        public Destructor() {
+            Console.WriteLine("Default constructor");
+
+        }
+
+         ~Destructor() {
+            Console.WriteLine("The destructor is also invoked implicitly,which is used to destroy the objects");
+        }
+
+        public static void Main(string[] args) { 
+            Destructor destructor = new Destructor();
+            Destructor destructor1 = new Destructor();
+           /* destructor = null;
+            destructor1 = null;
+            GC.Collect();
+            GC.WaitForPendingFinalizers(); */
+           
+        }
+    }
+}
