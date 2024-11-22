050707b2d0ee403377fa370c41fee97c8e293d4f
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-22 22:12:05 +0530

    multiLevelInheritance

---
+++ Basics/Oops/Inheritance/VehicleMultiLevelInheritance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.Oops.Inheritance
+{
+     class VehicleMultiLevelInheritance
+    {
+        public void Color()
+        {
+            Console.WriteLine("The color of the vehicle is red");
+        }
+    }
+
+    class Car : VehicleMultiLevelInheritance
+    {
+        public void CarMakesSound()
+        {
+            Console.WriteLine("sound");
+        }
+    }
+
+    class Bike : Car { 
+        public void BikeSpeed()
+        {
+            Console.WriteLine("bike speedometer");
+        }
+        public static void Main(string[] args)
+        {
+            Bike bike = new Bike();
+            bike.Color();
+            bike.CarMakesSound();
+            bike.BikeSpeed();
+
+        }
+    }
+
+
+    
+}
