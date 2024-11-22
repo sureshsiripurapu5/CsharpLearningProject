0debbacc89f29168e58fe1182c5f2f36359dcd7d
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-22 22:29:21 +0530

    arrayDeclarationAndInitialization

---
+++ Basics/Arrays/ArrayDeclarationAndInitialization.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.Arrays
+{
+    internal class ArrayDeclarationAndInitialization
+    {
+        public static void Main(string[] args)
+        {
+            int[] arrayName = { 1, 2, 3, 4, 5 };
+
+            //traversingg the array
+            for (int i = 0; i < arrayName.Length; i++)
+            {
+                Console.WriteLine(arrayName[i]);
+            }
+
+            //traversiong using for each loop
+            foreach (int item in arrayName)
+                { Console.WriteLine(item); }
+            
+                
+            
+        }
+    }
+}
