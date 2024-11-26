bfc7e3286d4d4e34efb8080983b892841976df85
sureshsiripurapu5 <139056065+sureshsiripurapu5@users.noreply.github.com>
2024-11-26 16:34:14 +0530

    2dArray

---
+++ Basics/Arrays/TwoDimensionalArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpLearningProject.Basics.Arrays
+{
+    internal class TwoDimensionalArray
+    {
+        public static void Main(string[] args)
+        {
+            //in java we use [] [] but here we have to give like [,] and size[2,3]
+            //declaration
+            int[,] arr1 = new int[3, 3];
+
+            arr1[0,0] = 10;
+            arr1[0,1] = 20;
+            arr1[0,2] = 30;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+
+                    Console.Write(arr1[i, j]+" ");
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
